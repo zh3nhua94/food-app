8dc3872951060ff60f9d6fb87adbb655301a889a
Zen Wong <zh3nhua94@gmail.com>
2023-11-03 17:40:25 +0800

    solve vercel fetch attempt9

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -5,9 +5,12 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
 	try {
 		const categories = await prisma.category.findMany();
-		const response = new NextResponse(JSON.stringify(categories), { status: 200 });
-		response.headers.set("content-type", "application/json");
-		return response;
+
+		// const response = new NextResponse(JSON.stringify(categories), { status: 200 });
+		// response.headers.set("content-type", "application/json");
+		// return response;
+
+		return NextResponse.json(categories, { status: 200 });
 	} catch (error) {
 		console.log(error);
 		return new NextResponse(JSON.stringify({ message: "Something went wrong!" }), { status: 500 });
