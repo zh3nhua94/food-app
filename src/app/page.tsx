de71c687a53f09e7cd290bf35bc615eb7812c348
Zen Wong <zh3nhua94@gmail.com>
2023-11-03 17:05:16 +0800

    solve vercel fetch attempt6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,20 @@ import Slider from "@/components/Slider";
 // import axios from "axios";
 
 export default async function Home() {
-	const getData = async () => {
-		// const res = await axios.get(process.env.NEXT_PUBLIC_API_URL + "/api/products");
-		const res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/products");
+	// const getData = async () => {
+	// 	// const res = await axios.get(process.env.NEXT_PUBLIC_API_URL + "/api/products");
+	// 	const res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/products");
 
-		if (!res.ok) {
-			throw new Error("Failed to retrieve items");
-		}
-		const resultTest = await res.text();
-		const result = await JSON.parse(resultTest);
-		console.log(result);
-		return result;
+	// 	if (!res.ok) {
+	// 		throw new Error("Failed to retrieve items");
+	// 	}
+	// 	const resultTest = await res.text();
+	// 	const result = await JSON.parse(resultTest);
+	// 	console.log(result);
+	// 	return result;
 
-		// return res.data;
-	};
+	// 	// return res.data;
+	// };
 	// const featuredProducts = await getData();
 
 	return (
